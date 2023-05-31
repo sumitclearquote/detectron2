30a1156f0c40fc5ad43f65736b979aecaf5c080c
kh-ld <65332353+kh-ld@users.noreply.github.com>
2023-05-31 13:10:27 +0530

    Removed dependencies from setup.py

--- setup.py
+++ setup.py
@@ -162,49 +162,49 @@ setup(
         # To tell if a package is pure-python, go to https://pypi.org/project/{name}/#files
         "Pillow>=7.1",  # or use pillow-simd for better performance
         "matplotlib",  # TODO move it to optional after we add opencv visualization
-        "pycocotools>=2.0.2",  # corresponds to https://github.com/ppwwyyxx/cocoapi
+        #"pycocotools>=2.0.2",  # corresponds to https://github.com/ppwwyyxx/cocoapi
         # Do not add opencv here. Just like pytorch, user should install
         # opencv themselves, preferrably by OS's package manager, or by
         # choosing the proper pypi package name at https://github.com/skvark/opencv-python
         # The following are pure-python dependencies that should be easily installable
-        "termcolor>=1.1",
-        "yacs>=0.1.8",
-        "tabulate",
-        "cloudpickle",
-        "tqdm>4.29.0",
-        "tensorboard",
+        #"termcolor>=1.1",
+        #"yacs>=0.1.8",
+        #"tabulate",
+        #"cloudpickle",
+        #"tqdm>4.29.0",
+        #"tensorboard",
         # Lock version of fvcore/iopath because they may have breaking changes
         # NOTE: when updating fvcore/iopath version, make sure fvcore depends
         # on compatible version of iopath.
         "fvcore>=0.1.5,<0.1.6",  # required like this to make it pip installable
         "iopath>=0.1.7,<0.1.10",
-        "future",  # used by caffe2
-        "pydot",  # used to save caffe2 SVGs
-        "dataclasses; python_version<'3.7'",
-        "omegaconf>=2.1",
-        "hydra-core>=1.1",
-        "black==22.3.0",
-        "timm",
-        "fairscale",
-        "packaging",
+        #"future",  # used by caffe2
+        #"pydot",  # used to save caffe2 SVGs
+        #"dataclasses; python_version<'3.7'",
+        #"omegaconf>=2.1",
+        #"hydra-core>=1.1",
+        #"black==22.3.0",
+        #"timm",
+        #"fairscale",
+        #"packaging",
         # If a new dependency is required at import time (in addition to runtime), it
         # probably needs to exist in docs/requirements.txt, or as a mock in docs/conf.py
     ],
     extras_require={
         # optional dependencies, required by some features
         "all": [
-            "scipy>1.5.1",
-            "shapely",
-            "pygments>=2.2",
-            "psutil",
-            "panopticapi @ https://github.com/cocodataset/panopticapi/archive/master.zip",
+         #   "scipy>1.5.1",
+         #   "shapely",
+         #   "pygments>=2.2",
+         #   "psutil",
+         #   "panopticapi @ https://github.com/cocodataset/panopticapi/archive/master.zip",
         ],
         # dev dependencies. Install them by `pip install 'detectron2[dev]'`
         "dev": [
-            "flake8==3.8.1",
-            "isort==4.3.21",
-            "flake8-bugbear",
-            "flake8-comprehensions",
+          #  "flake8==3.8.1",
+           # "isort==4.3.21",
+           # "flake8-bugbear",
+           # "flake8-comprehensions",
         ],
     },
     ext_modules=get_extensions(),
