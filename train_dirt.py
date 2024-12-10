e75f9ceb0d5ef783b16cefa6d90958a585d75e90
Sumit <sumit@clearquote.io>
2024-12-10 23:02:44 +0530

    fn

--- train_dirt.py
+++ train_dirt.py
@@ -131,12 +131,12 @@ def setup():
 
 	cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS = False #Keeping this FALSE includes images with empty annotations in training.
 	cfg.SOLVER.BASE_LR = 0.0000125 # divide 0.00125(default) by 2 since bsize was / 2 and further divide it by 4 since we're starting from previous best model
-	cfg.SOLVER.CHECKPOINT_PERIOD = 500 #2000 # =========================== 
+	cfg.SOLVER.CHECKPOINT_PERIOD = 2000 # =========================== 
 	cfg.SOLVER.LR_SCHEDULER_NAME = 'Cosine'
 	cfg.SOLVER.IMS_PER_BATCH = 4 # =========================== 5 epochs
-	cfg.SOLVER.MAX_ITER = 1000 #16000 #===================================== 50 epochs=27000 iters with 16 Batch size. 16000 iters = ~15 epoch
-	cfg.SOLVER.WARMUP_ITERS = 50 #500
-	cfg.TEST.EVAL_PERIOD = 500 #2000 # =========================== same as 'cfg.SOLVER.CHECKPOINT_PERIOD'
+	cfg.SOLVER.MAX_ITER = 16000 #===================================== 50 epochs=27000 iters with 16 Batch size. 16000 iters = ~15 epoch
+	cfg.SOLVER.WARMUP_ITERS = 500
+	cfg.TEST.EVAL_PERIOD = 2000 # =========================== same as 'cfg.SOLVER.CHECKPOINT_PERIOD'
 
 	cfg.OUTPUT_DIR = exp_dir #./mahindra_dirt/exp2
 
