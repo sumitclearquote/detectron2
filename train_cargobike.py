f0eaa730f3975026be5d100e22bf38c1de2ae92e
Sumit <sumit@clearquote.io>
2025-02-22 00:36:44 +0530

    hwr

--- train_cargobike.py
+++ train_cargobike.py
@@ -53,7 +53,7 @@ class_list = ['batterybox', 'cabinroof', 'frontbumper', 'frontws', 'leftcabcorne
 # class_list =  ['tyre', 'alloywheel', 'batterybox','cabinroof','frontbumper','frontws', 'rearroofside', 'tailgate',
 #               'cabcorner', 'frontwa', 'headlamp', 'orvm', 'roofside', 'crashguard', 'sidewall'] # exp2 class_list (left and right merged)
 
-exp_name = "exp5"
+exp_name = "exp6"
 exp_dir = f'./cargobike_training_dir/{exp_name}'
 
 dataset_path = "datasets/cargo_bike_training_data"
@@ -129,7 +129,7 @@ def setup():
 	cfg.MODEL.ANCHOR_GENERATOR.ASPECT_RATIOS = [[0.5, 1.0, 1.33, 1.5, 2.0]]
 	
 
-	cfg.MODEL.WEIGHTS = 'cargobike_training_dir/exp1/model_final.pth'
+	cfg.MODEL.WEIGHTS = 'cargobike_training_dir/exp5/model_final.pth'
 	#cfg.MODEL.WEIGHTS = 'model_final_a3ec72.pkl'
 	#Let training initialize from pre-trained
 
@@ -146,11 +146,11 @@ def setup():
 	# cfg.INPUT.MAX_SIZE_TEST = 800
 
 	cfg.DATALOADER.FILTER_EMPTY_ANNOTATIONS = True #Keeping this FALSE includes images with empty annotations in training.
-	cfg.SOLVER.BASE_LR = 0.004 #0.004 #=0.0002  #0.000275, 0.005  # considering default lr for bsize 16 is 0.02. 0.02/4 = 0.005. 0.0015/4
+	cfg.SOLVER.BASE_LR = 0.00001 #0.004 #=0.0002  #0.000275, 0.005  # considering default lr for bsize 16 is 0.02. 0.02/4 = 0.005. 0.0015/4
 	
 	cfg.SOLVER.LR_SCHEDULER_NAME = 'Cosine'
 	cfg.SOLVER.IMS_PER_BATCH = 4 # =========================== batch size. Default for detectron2 is 16
-	cfg.SOLVER.MAX_ITER = 9000 #===================================== 15 epochs for exp3. 30 epochs=8500 iters with 4 Batch size, 1095 images.
+	cfg.SOLVER.MAX_ITER = 8000 #===================================== 15 epochs for exp3. 30 epochs=8500 iters with 4 Batch size, 1095 images.
 	cfg.SOLVER.WARMUP_ITERS = 250
 	cfg.SOLVER.CHECKPOINT_PERIOD = 500 # =========================== ~every 2 epochs
 	cfg.TEST.EVAL_PERIOD = 500 # =========================== can keep same as 'cfg.SOLVER.CHECKPOINT_PERIOD'.
